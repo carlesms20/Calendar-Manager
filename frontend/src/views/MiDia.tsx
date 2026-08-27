8bb5b5e5c17974a71502ff1c3c1cfe1993a6093b
Carles Mari Segui <CMARSEG2@upv.edu.es>
2026-08-27 09:35:45 +0200

    Fix leve

--- frontend/src/views/MiDia.tsx
+++ frontend/src/views/MiDia.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FileText, Sparkles } from "lucide-react";
+import { Sparkles } from "lucide-react";
 import Chat from "../components/Chat";
 import Composer from "../components/Composer";
 import FocusDelDia from "../components/FocusDelDia";
